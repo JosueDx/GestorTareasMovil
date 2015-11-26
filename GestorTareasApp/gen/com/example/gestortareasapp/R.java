eef3b7e0957ca599a8cbf24c3505f9164649af30
JosueDx <josuepambeer@gmail.com>
2015-11-26 11:43:57 -0500

    segundo archivo

--- GestorTareasApp/gen/com/example/gestortareasapp/R.java
+++ GestorTareasApp/gen/com/example/gestortareasapp/R.java
@@ -36,6 +36,8 @@ public final class R {
         public static final int ic_op=0x7f020008;
     }
     public static final class id {
+        public static final int EdittextUser=0x7f090003;
+        public static final int EditttextPassword=0x7f090004;
         public static final int LinearLayout1=0x7f090000;
         public static final int Rapellido=0x7f090009;
         public static final int Rcedula=0x7f09000a;
@@ -49,9 +51,7 @@ public final class R {
         public static final int action_settings=0x7f090010;
         public static final int button1=0x7f090005;
         public static final int button2=0x7f090006;
-        public static final int email=0x7f090003;
         public static final int imageView1=0x7f090001;
-        public static final int password=0x7f090004;
         public static final int textView1=0x7f090002;
     }
     public static final class layout {
